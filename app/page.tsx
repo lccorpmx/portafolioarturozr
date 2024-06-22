f5c4cf1c8612db0d473efbcfefcbc141dcda1cbf
lccorpmx <lcccorpmx@gmail.com>
2024-06-22 10:20:51 -0600

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,201 @@
 import Image from "next/image";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { FcApproval } from "react-icons/fc";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithubSquare } from "react-icons/fa";
+import Link from 'next/link';
+import { RiSeparator } from "react-icons/ri";
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="p-12 flex flex-col gap-28">
+      <div className="">
+        <div className="m-4 p-2 flex flex-row items-center justify-center gap-8">
+          <div className="flex justify-end">
+            <Avatar className="w-56 h-56 shadow-green-400 shadow-2xl">
+              <AvatarImage src="/fotoazr.svg" />
+              <AvatarFallback>AZR</AvatarFallback>
+            </Avatar>
+          </div>
+          <div className="col-span-2 flex flex-col">
+            <div className="flex flex-row items-center gap-2 pb-4">
+              <h1 className="text-6xl font-bold">Welcome, I'm Arturo</h1>
+              <FcApproval className="w-8 h-8"/>
+            </div>
+            <h2 className="text-4xl pl-6 pb-2">Software Engineer</h2>
+            <h3 className="text-2xl pl-6 pb-2">Frontend Developer, Data Science Trainee & Designer</h3>
+            <p className="pl-6 text-xl">I develop web applications using Next.js and Tailwind,<br />with a keen interest in Artificial Intelligence.</p>
+            <div className="pl-6 flex gap-2 text-4xl pt-2">
+              <Link href={"https://www.linkedin.com/in/arturo-zilli-rios-7b6019315/"}>
+                <div>
+                  <FaLinkedin className="text-linkedin-blue hover:text-linkedin-dark-blue"/>
+                </div>
+              </Link>
+
+              <Link href={"https://github.com/lccorpmx"}>
+                <div>
+                  <FaGithubSquare className="text-white hover:text-gray-400"/>
+                </div>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="flex justify-center"><h2 className="text-5xl pb-6">Experience</h2></div>
+        <div className="flex justify-center">
+          <div className="flex justify-center items-center flex-col text-center">
+            <span className="text-3xl text-green-400">CannAssist AI: Artificial Intelligence in Endocannabinology</span>
+            <p className="text-lg">Comprehensive project on data engineering, encompassing the entire cycle<br />from data analysis to data science, as well as full-stack development and deployment on Kubernetes.</p>
+            <RiSeparator className="text-green-400 text-5xl"/>
+          </div>
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div>
+      <div className="flex justify-center"><h2 className="text-5xl pb-6">Skills</h2></div>
+      <div className="grid grid-cols-6 gap-6 items-center justify-items-center pr-56 pl-56">
+        <div>
+          <Image src={"/nextjs.svg"} width={150} height={150} alt={"cepa"}/>
+        </div>
+        <div>
+          <Image src={"/tailwind.svg"} width={120} height={120} alt={"cepa"}/>
+        </div>
+        <div>
+          <Image src={"/python.svg"} width={120} height={120} alt={"cepa"}/>
+        </div>
+        <div>
+          <Image src={"/fastapi.svg"} width={120} height={120} alt={"cepa"}/>
+        </div>
+        <div>
+          <Image src={"/sklearn.svg"} width={120} height={120} alt={"cepa"}/>
+        </div>
+        <div>
+          <Image src={"/pandas.svg"} width={120} height={120} alt={"cepa"}/>
+        </div>
+        <div>
+          <Image src={"/nunpy.svg"} width={120} height={120} alt={"cepa"}/>
+        </div>
+        <div>
+          <Image src={"/js.svg"} width={120} height={120} alt={"cepa"}/>
+        </div>
+        <div>
+          <Image src={"/sql.svg"} width={120} height={120} alt={"cepa"}/>
+        </div>
+        <div>
+          <Image src={"/prisma.svg"} width={120} height={120} alt={"cepa"}/>
+        </div>
+        <div>
+          <Image src={"/docker.svg"} width={120} height={120} alt={"cepa"}/>
+        </div>
+        <div>
+          <Image src={"/kubernetes.svg"} width={120} height={120} alt={"cepa"}/>
+        </div>
+        <div>
+          <Image src={"/supabase.svg"} width={120} height={120} alt={"cepa"}/>
+        </div>
+        <div>
+          <Image src={"/canva.svg"} width={120} height={120} alt={"cepa"}/>
+        </div>
+        <div>
+          <Image src={"/figma.svg"} width={120} height={120} alt={"cepa"}/>
+        </div>
+        <div>
+          <Image src={"/adobe.svg"} width={120} height={120} alt={"cepa"}/>
+        </div>
+      </div>
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <div>
+        <div className="flex justify-center"><h2 className="text-5xl">Projects</h2></div>
+        <div className="grid grid-cols-2 gap-4 pt-6 pr-56 pl-56">
+        <section className="overflow-hidden relative rounded-xl m-12  shadow-green-400 shadow-2xl">
+                            <div className='flex flex-col backdrop-blur-3xl overflow-hidden'>
+                                <div className='flex items-center justify-between py-4 px-6'>
+                                    <div className='truncate'>
+                                        <a href={"w"} target='_blank' rel='noreferrer'>
+                                            <h1 className='text-dark dark:text-white font-semibold text-2xl z-20 hover:underline'>CannAsisst</h1>
+                                        </a>
+                                        <p className='text-dark/60 mix-blend-darken dark:text-inherit text-md'>First medicinal cannabis artificial intelligence in Mexico.</p>
+                                    </div>
+                                </div>
+                                <div className='shadow-md'>
+                                    <Image
+                                        src={"/cannasisst.png"}
+                                        priority={true}
+                                        className='rounded-xl'
+                                        alt={"CannAsisst"}
+                                        width={400}
+                                        height={200}
+                                        sizes="100vw"
+                                        style={{
+                                            width: "100%",
+                                            height: "auto",
+                                            objectFit: "contain"
+                                        }} />
+                                </div>
+                            </div>
+        </section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        <section className="overflow-hidden relative rounded-xl m-12  shadow-green-400 shadow-2xl">
+                            <div className='flex flex-col backdrop-blur-3xl overflow-hidden'>
+                                <div className='flex items-center justify-between py-4 px-6'>
+                                    <div className='truncate'>
+                                        <a href={"w"} target='_blank' rel='noreferrer'>
+                                            <h1 className='text-dark dark:text-white font-semibold text-2xl z-20 hover:underline'>WeeHub</h1>
+                                        </a>
+                                        <p className='text-dark/60 mix-blend-darken dark:text-inherit text-md'>It's an app for recreational cannabis in Mexico.</p>
+                                    </div>
+                                </div>
+                                <div className='shadow-md'>
+                                    <Image
+                                        src={"/weehub.png"}
+                                        priority={true}
+                                        className='rounded-xl'
+                                        alt={"weehub"}
+                                        width={400}
+                                        height={200}
+                                        sizes="100vw"
+                                        style={{
+                                            width: "100%",
+                                            height: "auto",
+                                            objectFit: "contain"
+                                        }} />
+                                </div>
+                            </div>
+        </section>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <section className="overflow-hidden relative rounded-xl m-12  shadow-green-400 shadow-2xl">
+                            <div className='flex flex-col backdrop-blur-3xl overflow-hidden'>
+                                <div className='flex items-center justify-between py-4 px-6'>
+                                    <div className='truncate'>
+                                        <a href={"w"} target='_blank' rel='noreferrer'>
+                                            <h1 className='text-dark dark:text-white font-semibold text-2xl z-20 hover:underline'>LCCDice</h1>
+                                        </a>
+                                        <p className='text-dark/60 mix-blend-darken dark:text-inherit text-md'>It's an assistant app connected to OpenAI.</p>
+                                    </div>
+                                </div>
+                                <div className='shadow-md'>
+                                    <Image
+                                        src={"/lccdice.png"}
+                                        priority={true}
+                                        className='rounded-xl'
+                                        alt={"weehub"}
+                                        width={400}
+                                        height={200}
+                                        sizes="100vw"
+                                        style={{
+                                            width: "100%",
+                                            height: "auto",
+                                            objectFit: "contain"
+                                        }} />
+                                </div>
+                            </div>
+        </section>
+        </div>
       </div>
+
     </main>
   );
 }
